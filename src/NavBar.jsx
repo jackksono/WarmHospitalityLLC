e157370806f59b265c66899cf3dce5415da82b70
Jack Sonoda <jackksono@gmail.com>
2023-11-29 15:05:58 -0800

    Hover effects

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -18,10 +18,10 @@ const NavBar = () => {
         <div className='relative bg-fixed bg-center bg-cover h-36 bg-BG'>
             <div className='absolute inset-0 bg-color-BG bg-opacity-5 '>
                 <div className='flex items-center justify-center space-x-5 font-Raleway'>
-                    <Link to='/' className='z-20'>HOME</Link>
-                    <Link to='/about' className='z-20'>ABOUT</Link>
-                    <Link to='/services' className='z-20'>SERVICES</Link>
-                    <Link to='/contact' className='z-20'>CONTACT</Link>
+                    <Link to='/' className='z-20 hover:text-white'>HOME</Link>
+                    <Link to='/about' className='z-20 hover:text-white'>ABOUT</Link>
+                    <Link to='/services' className='z-20 hover:text-white'>SERVICES</Link>
+                    <Link to='/contact' className='z-20 hover:text-white'>CONTACT</Link>
                 </div>
                 <div className='absolute z-10 flex items-center justify-center -mt-6 transform -translate-x-1/2 left-1/2 bg-opacity-60'>
                     <Link to='/'>
