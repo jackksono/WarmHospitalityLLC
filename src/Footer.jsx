0407ef822acb3a812364f2d970ce8b8b19ef55ea
Jack Sonoda <jackksono@gmail.com>
2023-12-19 23:17:10 -0800

    Starting footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import logo from '../src/images/logo/1 (4).png'
 
 const Footer = () => {
   return (
+    <>
+    <div className='items-center justify-center w-50 h-50'>
+      <img src={logo} alt='logo'></img>
+    </div>
     <div>Footer</div>
+    </>
   )
 }
 
