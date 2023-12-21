1559502ffe4c97512d4690637723fabefa940e92
Jack Sonoda <jackksono@gmail.com>
2023-12-20 23:33:29 -0800

    Updating footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -7,7 +7,15 @@ const Footer = () => {
     <div className='items-center justify-center w-50 h-50'>
       <img src={logo} alt='logo'></img>
     </div>
-    <div>Footer</div>
+    <div> 
+      <h1>Check Out Our Links Here!</h1>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    <div>
+      
+    </div>
     </>
   )
 }
