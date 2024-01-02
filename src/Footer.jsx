e758f90e64fefe1f697bdbd5def859d10e26f85f
Jack Sonoda <jackksono@gmail.com>
2024-01-01 21:39:48 -0800

    Updates to footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <>
     <footer className="absolute mt-80 inset-x-0 bottom-0 w-screen h-[75px] lg:h-[200px]">
       <div className='items-center justify-center w-50 h-50'>
-        <img src={logo} alt='logo'></img>
+        <img className='h-50 w-50' src={logo} alt='logo'></img>
       </div>
       <div className='flex flex-col'> 
         <h1>Follow Us on Social Media!</h1>
@@ -17,10 +17,6 @@ const Footer = () => {
         <a href='https://Tiktok.com/WarmHospitalityGroup' target="_blank" rel="noopener noreferrer">
           <FaTiktok className='cursor-pointer'/>
         </a>
-
-      </div>
-      <div>
-        
       </div>
     </footer>
     </>
