fe027ec5c363fc36987d659880a41e35a4386fd2
Jack Sonoda <jackksono@gmail.com>
2024-01-08 19:08:18 -0800

    resize

--- src/Home.jsx
+++ src/Home.jsx
@@ -32,10 +32,10 @@ const Home = () => {
                 <div className='fixed inset-0 flex items-center justify-center bg-color-BG bg-opacity-60'>
                     <div className='flex flex-row justify-center mt-60 '>
                         <div className='parallax-element ' data-speed='0.05'>
-                            <img className='h-[850px] sm:w-[500px] 2xl:w-[700px] mix-blend-multiply' src={imageOne} alt='imageOne' />
+                            <img className='sm:h-[850px] sm:w-[500px] 2xl:w-[700px] mix-blend-multiply' src={imageOne} alt='imageOne' />
                         </div>
                         <div className='parallax-element' data-speed='0.05'>
-                            <img className='h-[850px] sm:w-[500px] 2xl:w-[700px] mix-blend-multiply' src={imageTwo} alt='imageTwo' />
+                            <img className='sm:h-[850px] sm:w-[500px] 2xl:w-[700px] mix-blend-multiply' src={imageTwo} alt='imageTwo' />
                         </div>
                     </div>
                 </div>
